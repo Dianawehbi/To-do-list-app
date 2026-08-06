665659a34b7dc054047f30c3d0369437cd188bd5
Diana Wehbe <dianawehbiii@gmail.com>
2026-08-06 10:49:14 +0300

    add main-layout component

---
+++ web-app/src/app/main-layout/main-layout.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { HeaderComponent } from '../features/shared/header/header';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './main-layout.html',
+})
+export class MainLayoutComponent {
+  private authService = inject(AuthService);
+
+  currentUser = this.authService.currentUser; 
+
+  onLogout(): void {
+    this.authService.logout();
+  }
+}
