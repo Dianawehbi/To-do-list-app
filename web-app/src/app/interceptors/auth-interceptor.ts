28cb4ec96074e916f2f5f5609200f2da91e486e1
Diana Wehbe <dianawehbiii@gmail.com>
2026-08-06 10:27:23 +0300

    Create auth-interceptor.ts

---
+++ web-app/src/app/interceptors/auth-interceptor.ts
@@ -0,0 +1,52 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, switchMap, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+const AUTH_ENDPOINTS = ['/api/auth/login', '/api/auth/refresh', '/api/auth/register'];
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const isAuthEndpoint = AUTH_ENDPOINTS.some((url) => req.url.includes(url));
+
+  const token = authService.accessToken();
+  const authReq =
+    token && !isAuthEndpoint
+      ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
+      : req;
+
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      // Don't attempt a refresh loop on the refresh/login calls themselves
+      console.log('Interceptor file error : ');
+      console.log(error);
+      if (isAuthEndpoint) {
+        return throwError(() => error);
+      }
+
+      // logout + refresh + ststua : 404
+
+      // Only handle 401 globally
+      if (error.status !== 401) {
+        router.navigate(['/login']);
+        return throwError(() => error);
+      }
+
+      return authService.refreshAccessToken().pipe(
+        switchMap(() => {
+          const newToken = authService.accessToken();
+          const retriedReq = req.clone({
+            setHeaders: { Authorization: `Bearer ${newToken}` },
+          });
+          return next(retriedReq);
+        }),
+        catchError((refreshError) => {
+          authService.logout();
+          return throwError(() => refreshError);
+        }),
+      );
+    }),
+  );
+};
